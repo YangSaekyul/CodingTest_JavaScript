97bee526be01e71114da0445e292bf275512f5d2
YangSaekyul <121498405+YangSaekyul@users.noreply.github.com>
2023-05-12 16:56:32 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var inputStr = fs.readFileSync('/dev/stdin').toString().trim();
+
+let a = parseInt(input[0]);
+
+console.log(a-543);
