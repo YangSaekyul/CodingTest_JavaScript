eff7219b04a75600cbc9d8693672ae61080b6526
YangSaekyul <121498405+YangSaekyul@users.noreply.github.com>
2023-05-12 16:38:04 +0900

    [Bronze V] Title: A×B, Time: 120 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+// var a = parseInt(input[0]);
+// var b = parseInt(input[1]);
+
+let a = input[0];
+let b = input[1];
+
+console.log(a * b);
