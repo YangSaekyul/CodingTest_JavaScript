58915126d875841b1b078e6e542290dff11c7afd
YangSaekyul <121498405+YangSaekyul@users.noreply.github.com>
2023-05-12 23:31:51 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var inputStr = fs.readFileSync('/dev/stdin').toString().trim();
+
+// let a = parseInt(input[0]);
+// let b = parseInt(input[1]);
+// let c = parseInt(input[2]);
+
+console.log("\\    /\\");
+console.log(" )  ( ')");
+console.log("(  /  )");
+console.log(" \\(__)|");
