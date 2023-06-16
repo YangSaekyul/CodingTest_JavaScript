2a549bd7b781e8585916f32925aef3fccbafc974
YangSaekyul <121498405+YangSaekyul@users.noreply.github.com>
2023-06-16 09:44:25 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,3 @@
 function solution(box, n) {
-  var boxWidth = Math.floor(box[0] / n);
-  var boxHeight = Math.floor(box[1] / n);
-  var boxDepth = Math.floor(box[2] / n);
-
-  return boxWidth * boxHeight * boxDepth;
+  return Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
 }
