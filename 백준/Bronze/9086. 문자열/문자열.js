8241dd35ee263573937239be015c52e5c49f5a5e
Saekyul Yang <121498405+YangSaekyul@users.noreply.github.com>
2023-07-19 23:34:24 +0900

    [Bronze V] Title: 문자열, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.js
@@ -0,0 +1,6 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+for (let i = 1; i < input.length; i++) {
+  console.log(input[i][0] + input[i][input[i].length - 1]);
+}
