bd117154bf2dddc778c4cf793329d92a600fe595
Saekyul Yang <121498405+YangSaekyul@users.noreply.github.com>
2023-07-19 19:37:59 +0900

    [Bronze V] Title: A+B - 8, Time: 140 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+// let input = fs.readFileSync("./input.txt").toString().split("\n");
+
+let testCase = Number(input[0]);
+let result = "";
+
+for (let i = 1; i <= testCase; i++) {
+  let num = input[i].split(" ");
+  result +=
+    `Case #${i}: ${Number(num[0])} + ${Number(num[1])} = ${
+      Number(num[0]) + Number(num[1])
+    }` + "\n";
+}
+
+console.log(result);
