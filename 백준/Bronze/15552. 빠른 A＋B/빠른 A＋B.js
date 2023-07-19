d83932036f59974f053f6406bfe29785852114f1
Saekyul Yang <121498405+YangSaekyul@users.noreply.github.com>
2023-07-19 18:46:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1272 ms, Memory: 168220 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+// let input = fs.readFileSync("./input.txt").toString().split("\n");
+
+let testCase = Number(input[0]);
+let result = "";
+
+for (let i = 1; i <= testCase; i++) {
+  let num = input[i].split(" ");
+  result += Number(num[0]) + Number(num[1]) + "\n";
+}
+
+console.log(result);
