84ebf71913b3a26359002364cedca1a85efb4bea
Saekyul Yang <121498405+YangSaekyul@users.noreply.github.com>
2023-07-20 00:33:00 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -3,16 +3,14 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let result = "";
 
-const length = input.length;
-
-for (let i = 1; i < length; i++) {
+for (let i = 0; i < input.length; i++) {
   let [a, b] = input[i].split(" ");
-  a = Number(a);
-
-  for (let j = 0; j < b.length; j++) {
-    result += b[j].repeat(a);
+  if (b) {
+    for (let j = 0; j < b.length; j++) {
+      result += b[j].repeat(a);
+    }
+    result += "\n";
   }
-  result += "\n";
 }
 
 console.log(result);
