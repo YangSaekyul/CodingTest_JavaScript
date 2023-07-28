fa5cb4c7d8dcc4d5ce6e994d55d0859fea44ceca
Saekyul Yang <121498405+YangSaekyul@users.noreply.github.com>
2023-07-28 19:35:01 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+for (let i = 0; i < input.length - 1; i++) {
+  let number = input[i].split(" ").map(Number);
+
+  if (number[1] % number[0] === 0) {
+    console.log("factor");
+  } else if (number[0] % number[1] === 0) {
+    console.log("multiple");
+  } else {
+    console.log("neither");
+  }
+}
